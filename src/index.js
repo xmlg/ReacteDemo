42e08d1f590cbb4c4357c017681901c09cd791c7
xumin <xumin@trs.com.cn>
2020-11-10 18:22:58 +0800

    添加小姐姐服务

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import App from './App'
+import XiaJieJie from './XiaoJieJie'
 //<App />:jsx is javascript and xml 遇到<就当作javascript来解析
 //自定义组件首字母大写 App 改为app会报错
-ReactDOM.render(<App />,document.getElementById('root'))
+ReactDOM.render(<XiaJieJie />,document.getElementById('root'))
