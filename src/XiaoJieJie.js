b0ce94bae34cdf92f59a79cf62aa1824554cddb0
xumin <xumin@trs.com.cn>
2020-11-10 18:23:32 +0800

    添加页面

---
+++ src/XiaoJieJie.js
@@ -0,0 +1,21 @@
+import React ,{Component,Fragment} from 'react'
+
+class XiaJieJie extends Component{
+    render(){
+        return (
+            // <div>
+            <Fragment>
+                <div>
+                    <input/>
+                    <button>增加服务</button>
+                </div>
+                <ul claaName='list'>
+                    <li>头部按摩</li>
+                    <li>脊椎按摩</li>
+                </ul>
+            </Fragment>
+            // </div>
+        )
+    }
+}
+export default XiaJieJie
