463464923ca68781419306287bddb3911206851a
xumin <xumin@trs.com.cn>
2020-11-11 18:12:09 +0800

    服务可以添加

--- src/XiaoJieJie.js
+++ src/XiaoJieJie.js
@@ -1,21 +1,43 @@
 import React ,{Component,Fragment} from 'react'
 
 class XiaJieJie extends Component{
+    constructor(props){
+        super(props)
+        this.state={
+            inputValue:"默认值",
+            list:['基础按摩','头部按摩']
+        }        
+    }
     render(){
         return (
             // <div>
             <Fragment>
                 <div>
-                    <input/>
-                    <button>增加服务</button>
+                    <input value={this.state.inputValue} onChange={this.inputChange.bind(this)}/>
+                    <button onClick={this.addList.bind(this)}>增加服务</button>
                 </div>
-                <ul claaName='list'>
-                    <li>头部按摩</li>
-                    <li>脊椎按摩</li>
+                <ul className='list'>
+                     {
+                        this.state.list.map((item,index)=>{
+                            return<li key={index+item}>{item}</li>
+                        })
+                     }   
                 </ul>
             </Fragment>
             // </div>
         )
+    }
+    inputChange(e){
+        console.log(e.target.value)
+        this.setState({
+            inputValue:e.target.value
+        })
+    }
+    addList(e){
+        this.setState({
+            list:[...this.state.list,this.state.inputValue],
+            inputValue:''
+        })
     }
 }
 export default XiaJieJie
--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import XiaJieJie from './XiaoJieJie'
+
 //<App />:jsx is javascript and xml 遇到<就当作javascript来解析
 //自定义组件首字母大写 App 改为app会报错
-ReactDOM.render(<XiaJieJie />,document.getElementById('root'))
+ReactDOM.render(< XiaJieJie/>,document.getElementById('root'))
